d91358695eb4d77d4d379bf45f01712b562bc6eb
MarkEduardovich <321yamaha123@gmail.com>
2023-01-28 23:05:43 +0200

    Initial first comit
    (setup constraints for views)

---
+++ FirstProlect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstProlect
+//
+//  Created by Mark on 13.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+   
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
